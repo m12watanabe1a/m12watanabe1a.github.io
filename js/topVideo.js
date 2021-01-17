55d069946db63bd2a0b1c282eedff5db0b072fb4
suzunedp <suzunedp@users.noreply.github.com>
2021-01-17 13:39:39 +0000

    deploy: HarvestX/hx_web2@f217ab0868368d5164033876cb0658f3801d5078

--- js/topVideo.js
+++ js/topVideo.js
@@ -3,28 +3,50 @@ const fileName = {
     lg: "hxtop.mp4",
     md: "hxtop_md.mp4",
     sm: "hxtop_sm.mp4"
-   }
-   
-   function getFilePath(fileName){
-     return  document.location.protocol + "//" + document.location.host + "/img/" + fileName
-   }
-   
-   const windowWidth = window.innerWidth
-   const windowHeight = window.innerHeight
+}
 
-   console.log(windowWidth)
-   console.log(windowHeight)
-   const topVideo = document.getElementById('hh-video')
+const breakpoint = {
+  md: 560,
+  lg: 1024
+}
 
-   if (windowWidth < 560 && windowHeight*0.9/windowWidth < 16/9 ){
-     topVideo.classList.add("resize-video")
-   }
-   
-   if (windowWidth >= 1025){
-     topVideo.src = getFilePath(fileName.lg)
-   } else if(windowWidth >= 560) {
-     topVideo.src = getFilePath(fileName.md)
-   } else {
-     topVideo.src = getFilePath(fileName.sm)
-   }
-   topVideo.load();
+const windowWidth = window.innerWidth
+const windowHeight = window.innerHeight
+
+const topVideo = document.getElementById('hh-video')
+
+window.addEventListener('load', function(){
+  loadVideo()
+  topVideo.muted = true
+  topVideo.loop = true
+})
+
+topVideo.addEventListener('canplaythrough', (event) => {
+  topVideo.play()
+})
+
+window.addEventListener('focus', (event) =>{
+  topVideo.play()
+});
+
+function getFilePath(fileName){
+  return  document.location.protocol + "//" + document.location.host + "/img/" + fileName
+}
+
+function adjustVideoSize(width, height){
+  if (width < 560 && height*0.9/width < 16/9 ){
+    topVideo.classList.add("resize-video")
+  }
+}
+
+function loadVideo(){
+  if (windowWidth > 1024){
+    topVideo.src = getFilePath(fileName.lg)
+  } else if(windowWidth >= 560) {
+    topVideo.src = getFilePath(fileName.md)
+  } else {
+    topVideo.src = getFilePath(fileName.sm)
+  }
+  topVideo.load()
+  adjustVideoSize(windowWidth, windowHeight)
+}
